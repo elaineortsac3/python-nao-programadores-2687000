1b7813ae8b035db0fe486a4cb25211d46a7eafa8
elaineortsac3 <elaineortsac3@gmail.com>
2024-08-15 22:43:01 +0000

    exerc3

--- exercicios/1-primeiros-passos/2-string.py
+++ exercicios/1-primeiros-passos/2-string.py
@@ -1,20 +1,20 @@
 resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por isso está estudando 'python'."
 
 # Imprima na tela a variável "resumo"
-
+print(resumo)
 
 # Imprima na tela apenas a segunda letra da variável
-
-
+print(resumo[1])
+print(resumo[:6])
 # Imprima na tela a idade de Paloma (resposta esperada: "46")
-
-
+print(resumo[31:])
+resumo.casefold
 # Imprima na tela o trecho final da variável
-
-
+resumo.capitalize
+resumo.count
 # Converta todos as letras para minúsculo e imprima na tela
-
-
+idade= 46
+print(type(idade))
 # Converta todas as letras para maiúscula e imprima na tela
 
 
@@ -25,4 +25,12 @@ resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por is
 
 
 # Imprima na tela uma string utilizando uma variável, usando o recurso string format
-
+idade= 10+23
+print(idade)
+divisao = 6/3
+print(divisao)
+decimal=(30/8)
+print(decimal)
+print(round(decimal,1))
+print(type(decimal))
+print(f'Paloma estudou {3+1} horas')
